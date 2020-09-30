f3d931880d58bbf7b2d9edd4e76c0fb8dfe488c9
Mok <nataegorova@MacBook.Dlink>
2020-09-30 11:30:25 +0300

    Добавлено приложение Users

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render, redirect
 
-from .models import Topic
-from .forms import TopicForm
-
-# Create your views here.
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
 
 
 def index(request):
@@ -41,3 +39,42 @@ def new_topic(request):
     # Вывести пустую или недействительную форму.
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+    """Добавляет новую запись по конкретной теме."""
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        # Данные не отправлялись, создается пустая форма.
+        form = EntryForm()
+    else:
+        # Отправлены данные POST; обработать данные.
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id=topic.id)
+
+    # Вывести пустую или недействительную форму.
+    context = {'topic': topic ,'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
+
+
+def edit_entry(request, entry_id):
+    """Редактирует существующую запись."""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        # Исходный запрос, форма заполняется данными текущей записи.
+        form = EntryForm(instance=entry)
+    else:
+        # Отправлены данные POST; обработать данные.
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topic', topic_id=topic.id)
+
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
